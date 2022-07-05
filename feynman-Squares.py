8b2dd928533c4367922211b28b512ac3743c32d1
Raghu <ilu.raghus@icloud.com>
2022-07-05 09:43:49 +0530

    Add files via upload

--- feynman-Squares.py
+++ feynman-Squares.py
@@ -1,5 +1,7 @@
+# def count_squares(n):
+#     sq=0
+#     for i in range(1,n+1):
+#         sq+=i*i
+#     return sq
 def count_squares(n):
-    sq=0
-    for i in range(1,n+1):
-        sq+=i*i
-    return sq
+    return int((n*(n+1)*((n<<1)+1)/6))
